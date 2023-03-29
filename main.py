f1c8e7c33fd31154f5c2cc29e6055527ada43316
yansansilva <86764284+yansansilva@users.noreply.github.com>
2023-03-29 18:37:12 -0300

    Update main.py

--- main.py
+++ main.py
@@ -80,7 +80,7 @@ def update_data():
         dados = []
         num_linhas = 0
         for i, row in enumerate(target_data):
-            target_time = datetime.datetime.strptime(row['DATA'][11:-3], '%H:%M').time()
+            target_time = datetime.datetime.strptime(row['DATA-RPI'][11:-3], '%H:%M').time()
             for filtered_row in filtered_data:
                 filtered_time = datetime.datetime.strptime(filtered_row['Hora'][11:-3], '%H:%M').time()
                 if filtered_time == target_time:
